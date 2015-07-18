20d7247ff4b4b5cffe06b8f2de8eebffce94b524
Marcio Hariki <marciohariki@gmail.com>
2015-07-18 03:01:05 -0300

    removing most of the cool stuff

--- Assets/Completed/Scripts/GameManager.cs
+++ Assets/Completed/Scripts/GameManager.cs
@@ -68,24 +68,7 @@ namespace Completed
 			//While doingSetup is true the player can't move, prevent player from moving while title card is up.
 			doingSetup = true;
 			
-			//Get a reference to our image LevelImage by finding it by name.
-			levelImage = GameObject.Find("LevelImage");
-			
-			//Get a reference to our text LevelText's text component by finding it by name and calling GetComponent.
-			levelText = GameObject.Find("LevelText").GetComponent<Text>();
-			
-			//Set the text of levelText to the string "Day" and append the current level number.
-			levelText.text = "Day " + level;
-			
-			//Set levelImage to active blocking player's view of the game board during setup.
-			levelImage.SetActive(true);
-			
-			//Call the HideLevelImage function with a delay in seconds of levelStartDelay.
-			Invoke("HideLevelImage", levelStartDelay);
-			
-			//Clear any Enemy objects in our List to prepare for next level.
-			enemies.Clear();
-			
+
 			//Call the SetupScene function of the BoardManager script, pass it current level number.
 			boardScript.SetupScene(level);
 			
