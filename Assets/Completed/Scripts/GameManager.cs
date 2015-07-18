e5aaad00933a857eaf90392302199d7262de5fe2
viceca <victor.nascimentobr@gmail.com>
2015-07-18 15:11:43 -0300

    Updated
    Udated level, moving and minor details

--- Assets/Completed/Scripts/GameManager.cs
+++ Assets/Completed/Scripts/GameManager.cs
@@ -9,14 +9,19 @@ namespace Completed
 	public class GameManager : MonoBehaviour
 	{
 		public float levelStartDelay = 2f;						//Time to wait before starting level, in seconds.
+		public float turnDelay = 10f;							//Delay between each Player turn.
+		public int playerFoodPoints = 100;						//Starting value for Player food points.
 		public static GameManager instance = null;				//Static instance of GameManager which allows it to be accessed by any other script.
 		[HideInInspector] public bool playersTurn = true;		//Boolean to check if it's players turn, hidden in inspector but public.
-				
+		
+		
 		private Text levelText;									//Text to display current level number.
 		private GameObject levelImage;							//Image to block out level as levels are being set up, background for levelText.
 		private BoardManager boardScript;						//Store a reference to our BoardManager which will set up the level.
+		private int level = 1;									//Current level number, expressed in game as "Day 1".
+		private bool enemiesMoving;								//Boolean to check if enemies are moving.
 		private bool doingSetup = true;							//Boolean to check if we're setting up board, prevent Player from moving during setup.
-		
+		public bool waiting_update = false;
 		
 		
 		//Awake is always called before any Start functions
@@ -36,7 +41,7 @@ namespace Completed
 			
 			//Sets this to not be destroyed when reloading scene
 			DontDestroyOnLoad(gameObject);
-
+			
 			//Get a component reference to the attached BoardManager script
 			boardScript = GetComponent<BoardManager>();
 			
@@ -47,6 +52,8 @@ namespace Completed
 		//This is called each time a scene is loaded.
 		void OnLevelWasLoaded(int index)
 		{
+			//Add one to our level number.
+			level++;
 			//Call InitGame to initialize our level.
 			InitGame();
 		}
@@ -56,7 +63,7 @@ namespace Completed
 		{
 			//While doingSetup is true the player can't move, prevent player from moving while title card is up.
 			doingSetup = true;
-
+			
 			//Get a reference to our image LevelImage by finding it by name.
 			levelImage = GameObject.Find("LevelImage");
 			
@@ -64,16 +71,16 @@ namespace Completed
 			levelText = GameObject.Find("LevelText").GetComponent<Text>();
 			
 			//Set the text of levelText to the string "Day" and append the current level number.
-			levelText.text = "Testing";
+			levelText.text = "Day " + level;
 			
 			//Set levelImage to active blocking player's view of the game board during setup.
 			levelImage.SetActive(true);
 			
 			//Call the HideLevelImage function with a delay in seconds of levelStartDelay.
 			Invoke("HideLevelImage", levelStartDelay);
-
+			
 			//Call the SetupScene function of the BoardManager script, pass it current level number.
-			boardScript.SetupScene(1);
+			boardScript.SetupScene(level);
 			
 		}
 		
@@ -92,19 +99,28 @@ namespace Completed
 		void Update()
 		{
 			//Check that playersTurn or enemiesMoving or doingSetup are not currently true.
-			if(playersTurn || doingSetup)
-				
+			if (playersTurn || doingSetup)
 				//If any of these are true, return and do not start MoveEnemies.
 				return;
-			
-			//Start moving enemies.
+			if (!waiting_update) {
+				StartCoroutine (PlayerTurnReset ());
+				waiting_update = true;
+			}
+
+		}
+
+		IEnumerator PlayerTurnReset () 
+		{
+			yield return new WaitForSeconds(turnDelay);
+			playersTurn = true;
+			waiting_update = false;
 		}
 
 		//GameOver is called when the player reaches 0 food points
 		public void GameOver()
 		{
 			//Set levelText to display number of levels passed and game over message
-			levelText.text = "After " + 1 + " days, you starved.";
+			levelText.text = "After " + level + " days, you starved.";
 			
 			//Enable black background image gameObject.
 			levelImage.SetActive(true);
@@ -112,6 +128,7 @@ namespace Completed
 			//Disable this GameManager.
 			enabled = false;
 		}
+
 	}
 }
 
