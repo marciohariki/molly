2beca501c26c59f0ccc03c5593d1aeada2fc4337
Marcio Hariki <marciohariki@gmail.com>
2015-07-30 20:03:51 -0300

    adding health

---
+++ Assets/Completed/Scripts/HealthManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthManager : MonoBehaviour {
+
+	public float scale = 0f;
+
+	public float barSize = -104f;
+
+	public RectTransform rectTransform;
+
+	void Update() {
+//		rectTransform.localPosition = new Vector3 ((1 - scale) * (-barSize), rectTransform.localPosition.y, rectTransform.localPosition.z);
+		rectTransform.localScale = new Vector3 (scale, rectTransform.localScale.y, rectTransform.localScale.z);
+
+	}
+	
+	public void getHit(string enemyTag){
+		print (enemyTag);
+		if (scale > 0) {
+			switch (enemyTag) {
+				
+			case "square":
+				scale -= 0.1f;
+				break;
+			case "circle":
+				scale -= 0.2f;
+				break;
+				
+			}
+		}
+		
+	}
+}
